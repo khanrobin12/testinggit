a56f2bfea5dd8aea39c77314ba16163bf10d3ca9
Ali Noor <ali.noor12ny@yahoo.com>
2023-01-24 18:16:20 -0500

    2nd commit for testing

--- src/test/java/com/testpackage/ClassA.java
+++ src/test/java/com/testpackage/ClassA.java
@@ -4,6 +4,8 @@ public class ClassA {
 
 	public static void main(String[] args) {
 		System.out.println("I am in calss a ");
+		System.out.println("Pushing 2nd time");
+		
 
 	}
 
--- src/test/java/com/testpackage/ClassB.java
+++ src/test/java/com/testpackage/ClassB.java
@@ -4,6 +4,7 @@ public class ClassB {
 
 	public static void main(String[] args) {
 		System.out.println("I am in class B");
+		System.out.println("pushing 2nd time");
 	}
 
 }
