cd41c541a8d2bf4408416cfa9758ea03a76714f0
Ali Noor <ali.noor12ny@yahoo.com>
2023-01-24 18:07:36 -0500

    first commit

---
+++ src/test/java/com/testpackage/ClassA.java
@@ -0,0 +1,10 @@
+package com.testpackage;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		System.out.println("I am in calss a ");
+
+	}
+
+}
---
+++ src/test/java/com/testpackage/ClassB.java
@@ -0,0 +1,9 @@
+package com.testpackage;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		System.out.println("I am in class B");
+	}
+
+}
