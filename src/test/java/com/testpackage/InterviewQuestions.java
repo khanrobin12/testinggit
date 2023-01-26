afc4f0ae9e94debb68c9b4afc65d790581fd4f17
Ali Noor <ali.noor12ny@yahoo.com>
2023-01-26 00:37:34 -0500

    Interview question ten

--- src/test/java/com/testpackage/InterviewQuestions.java
+++ src/test/java/com/testpackage/InterviewQuestions.java
@@ -5,25 +5,84 @@ import java.util.Scanner;
 public class InterviewQuestions {
 
 	public static void main(String[] args) {
-		// to print an integer enter by user. 
-		
+		// to print an integer enter by user.
+
 		/*
 		 * Scanner sc=new Scanner(System.in);
 		 * System.out.println("Please enter a number"); int a =sc.nextInt();
 		 * System.out.println(a);
 		 */
+
+		// to add two numbers
+		/*
+		 * int a,b; Scanner ab=new Scanner(System.in); Scanner ac=new
+		 * Scanner(System.in); System.out.println("Please enter two integer number");
+		 * a=ab.nextInt(); b=ac.nextInt();
+		 * 
+		 * System.out.println(a+b);
+		 */
+
+		// multiply two floating point numbers
+
+		/*
+		 * float a, b; Scanner ab = new Scanner(System.in); Scanner ac = new
+		 * Scanner(System.in); 
+		 * System.out.println("Please enter two float number");
+		 * 
+		 * a=ab.nextFloat(); b=ac.nextFloat();
+		 * 
+		 * System.out.println(a*b);
+		 */
 		
-		// to add two numbers 
-		int a,b;
-		Scanner ab=new Scanner(System.in);
-		Scanner ac=new Scanner(System.in);
-		System.out.println("Please enter two integer number");
-		a=ab.nextInt();
-		b=ac.nextInt();
+		// find ascii value of a character
 		
-		System.out.println(a+b);
+		/*
+		 * int askey='H'; System.out.println(askey);
+		 */
+		
+		//compute quotient and remainder 
+		/*
+		 * int a,b; a=65; b=10; int quotient=a/b; int reminder=a%b;
+		 * 
+		 * System.out.println("Quotient           "+quotient);
+		 * System.out.println("Reminder           "+reminder);
+		 */
+	
 		
 		
+		
+		//swap tow number 
+		
+		/*
+		 * int a,b,t; a=10; b=20; System.out.println("Before Swap"+a);
+		 * System.out.println("Before Swap"+b); t=a; System.out.println(t); a=b;
+		 * System.out.println(a); b=t; System.out.println(b);
+		 * 
+		 * a=a-b; b=a+b; a=b-a; System.out.println(a+"   "+b);
+		 */
+		
+		//check whether a number is even or odd. 
+		/*
+		 * int a=50; if (a%2==0) { System.out.println("Even");
+		 * 
+		 * }else { System.out.println("odd"); }
+		 */
+		//check whether an alphabet is vowel or consonant. 
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter a Charachater");
+		char a=sc.next().charAt(0);
+		System.out.println(a);
+		
+		if (a=='a'||a=='e'||a=='i'||a=='0'||a=='u') {
+			System.out.println("Vowel");
+			
+		}else {
+			System.out.println("Consonant");
+		}
+		//fine the largest amount three numbers
+		// find teh roots of a quadratic equation. 
+
 	}
 
 }
